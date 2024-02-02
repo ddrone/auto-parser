db9fdb01427724fa3b5b4417abccb243f8708f27
Andrew Shulaev <ash.drone@gmail.com>
2024-02-02 15:28:51 +0000

    Some sketches

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,14 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import GHC.Generics (Generic)
+
+data Tree a
+  = Node a [Tree a]
+  deriving (Generic)
+
+test :: Tree Int
+test = Node 1 [Node 2 [], Node 3 [Node 4 []], Node 5 []]
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
---
+++ src/AutoPrinter.hs
@@ -0,0 +1,1 @@
+module AutoPrinter where

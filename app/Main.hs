6e55368b0c22ee50c2d07ca76e7caf2cef0b7108
Andrew Shulaev <ash.drone@gmail.com>
2024-02-02 15:32:37 +0000

    Start working ot AutoPrinter

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
+-- import AutoPrinter
 import GHC.Generics (Generic)
 
 data Tree a
   = Node a [Tree a]
   deriving (Generic)
+
+-- instance ShowBuilder a => ShowBuilder (Tree a) where
 
 test :: Tree Int
 test = Node 1 [Node 2 [], Node 3 [Node 4 []], Node 5 []]
--- src/AutoPrinter.hs
+++ src/AutoPrinter.hs
@@ -1,1 +1,12 @@
 module AutoPrinter where
+
+import GHC.Generics
+import Data.Text.Lazy.Builder (Builder)
+
+class ShowBuilder a where
+  build :: a -> Builder
+  default build :: (Generic a, ShowBuilder1 (Rep a)) => a -> Builder
+  build = build1 . from
+
+class ShowBuilder1 f where
+  build1 :: f p -> Builder

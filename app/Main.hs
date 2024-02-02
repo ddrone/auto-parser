ff7fd7b748c369a4278553d63071d097d37c9e24
Andrew Shulaev <ash.drone@gmail.com>
2024-02-02 22:35:15 +0000

    Add some parentheses, they show up at the wrong places at the moment though

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,9 @@
 module Main where
 
 import AutoPrinter
+import Playground
 import GHC.Generics (Generic, from)
 import qualified Data.Text.Lazy.IO as TextIO
-
-data Tree a
-  = Leaf
-  | Node (Tree a) a (Tree a)
-  deriving (Generic, Show)
-
-data RoseTree a = Rose a [RoseTree a]
-  deriving (Generic, Show)
-
-instance ShowBuilder a => ShowBuilder (Tree a) where
-
-test :: Tree Int
-test = Node (Node Leaf 0 Leaf) 1 (Node (Node Leaf 2 Leaf) 3 Leaf)
-
-rose :: RoseTree Int
-rose = Rose 1 [Rose 2 [], Rose 3 [Rose 4 [], Rose 5 []], Rose 6 []]
-
-instance ShowBuilder a => ShowBuilder (RoseTree a) where
 
 main :: IO ()
 main = do
--- src/AutoPrinter.hs
+++ src/AutoPrinter.hs
@@ -7,49 +7,51 @@ import GHC.Generics
 import GHC.TypeLits (KnownSymbol, symbolVal)
 
 class ShowBuilder a where
-  build :: a -> Builder
-  default build :: (Generic a, ShowBuilder1 (Rep a)) => a -> Builder
-  build = build1 . from
+  build :: Bool -> a -> Builder
+  default build :: (Generic a, ShowBuilder1 (Rep a)) => Bool -> a -> Builder
+  build b = build1 b . from
 
 class ShowBuilder1 f where
-  build1 :: f p -> Builder
+  build1 :: Bool -> f p -> Builder
 
 instance ShowBuilder Int where
-  build = fromString . show
+  build _ = fromString . show
 
 instance ShowBuilder a => ShowBuilder [a] where
-  build ls = case ls of
+  build _ ls = case ls of
     [] -> fromString "[]"
-    [x] -> singleton '[' <> build x <> singleton ']'
+    [x] -> singleton '[' <> build False x <> singleton ']'
     x : rest -> singleton '[' <> go x rest
     where
-      go x [] = build x <> singleton ']'
-      go x (y : rest) = build x <> fromString ", " <> go y rest
+      go x [] = build False x <> singleton ']'
+      go x (y : rest) = build False x <> fromString ", " <> go y rest
 
 showText :: ShowBuilder a => a -> Text
-showText = toLazyText . build
+showText = toLazyText . build False
 
 instance ShowBuilder1 U1 where
   build1 _ = mempty
 
 instance ShowBuilder c => ShowBuilder1 (K1 i c) where
-  build1 (K1 x) = build x
+  build1 _ (K1 x) = build True x
 
 constructorName :: KnownSymbol s => M1 i (MetaCons s fi b) f a -> String
 constructorName (_ :: M1 i (MetaCons s fi b) f a) = symbolVal (Proxy :: Proxy s)
 
 instance (KnownSymbol s, ShowBuilder1 f) => ShowBuilder1 (M1 i (MetaCons s fi b) f) where
-  build1 y@(M1 x) = fromString (constructorName y) <> singleton ' ' <> build1 x
+  build1 b y@(M1 x) = wrap (fromString (constructorName y) <> build1 False x)
+    where
+      wrap builder = if b then singleton '(' <> builder <> singleton ')' else builder
 
 instance ShowBuilder1 f => ShowBuilder1 (M1 i (MetaData s1 s2 b1 b2) f) where
-  build1 (M1 x) = build1 x
+  build1 b (M1 x) = build1 b x
 
 instance ShowBuilder1 f => ShowBuilder1 (M1 i (MetaSel ms su ss ds) f) where
-  build1 (M1 x) = build1 x
+  build1 b (M1 x) = build1 b x
 
 instance (ShowBuilder1 f, ShowBuilder1 g) => ShowBuilder1 (f :+: g) where
-  build1 (L1 x) = build1 x
-  build1 (R1 x) = build1 x
+  build1 b (L1 x) = build1 b x
+  build1 b (R1 x) = build1 b x
 
 instance (ShowBuilder1 f, ShowBuilder1 g) => ShowBuilder1 (f :*: g) where
-  build1 (x :*: y) = build1 x <> build1 y
+  build1 b (x :*: y) = singleton ' ' <> build1 b x <> build1 b y

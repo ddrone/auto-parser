4cd046d6662a9719b32c6788654552e0bb6f7a5e
Andrew Shulaev <ash.drone@gmail.com>
2024-02-06 12:16:13 +0000

    More cases for datatype-generic parser

--- src/AutoParser.hs
+++ src/AutoParser.hs
@@ -1,11 +1,14 @@
 module AutoParser where
 
 import Control.Applicative ((<|>))
+import Data.Proxy
 import Data.Text (Text)
 import Data.Void (Void)
 import GHC.Generics
+import GHC.TypeLits (KnownSymbol, symbolVal)
 import Text.Megaparsec (Parsec)
 import Text.Megaparsec.Char (space1)
+import qualified Data.Text as Text
 import qualified Text.Megaparsec.Char.Lexer as Lexer
 
 type Parser = Parsec Void Text
@@ -34,6 +37,17 @@ instance TextParser c => TextParser1 (K1 i c) where
   parse1 = K1 <$> inner
     where
       inner = parse <|> (symbol "(" *> parse <* symbol ")")
+
+instance (KnownSymbol s, TextParser1 f) => TextParser1 (M1 i (MetaCons s fi b) f) where
+  parse1 = symbol (Text.pack constructorName) *> (M1 <$> parse1)
+    where
+      constructorName = symbolVal (Proxy :: Proxy s)
+
+instance TextParser1 f => TextParser1 (M1 i (MetaData s1 s2 b1 b2) f) where
+  parse1 = M1 <$> parse1
+
+instance TextParser1 f => TextParser1 (M1 i (MetaSel ms su ss ds) f) where
+  parse1 = M1 <$> parse1
 
 instance (TextParser1 f, TextParser1 g) => TextParser1 (f :+: g) where
   parse1 = (L1 <$> parse1) <|> (R1 <$> parse1)
